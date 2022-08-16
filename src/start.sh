e7e1bc1f696938e9136a1d726baeabdaad07a941
wchorski <wchorski@gmail.com>
2022-08-16 15:44:40 -0500

    new path /usr/bin/chromium-browser

--- src/start.sh
+++ src/start.sh
@@ -6,13 +6,13 @@ xset -dpms
 
 unclutter -idle 0.5 -root &
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/sign-admin/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/sign-admin/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/sign/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/sign/.config/chromium/Default/Preferences
 
-/usr/bin/chromium --noerrdialogs --disable-infobars --kiosk http://localhost:3000
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://nodesignage.williamusic.com/player
 
 ### simulate keypress (ctrl+R to refresh browser)
-# while true; do
-#    xdotool keydown ctrl+R; xdotool keyup ctrl+R;
-#    sleep 30
-# done
+while true; do
+   xdotool keydown ctrl+R; xdotool keyup ctrl+R;
+   sleep 69
+done
